7bdfb0145e93376aa5d0fd1f69cf66f746b029d0
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-02-20 20:03:39 -0500

    margin right reduced

--- js/suffixtree.js
+++ js/suffixtree.js
@@ -10,7 +10,7 @@ var treeData = stree.addString('MISSISSIPPI$').convertToJson();
 
 
 // ************** Generate the tree diagram	 *****************
-var margin = {top: 15, right: 45, bottom: 20, left: 50},
+var margin = {top: 5, right: 35, bottom: 20, left: 50},
 	width = 960 - margin.right - margin.left,
 	height = 800 - margin.top - margin.bottom;
 	
