8646e48e341770d9a0a17ef153094e59d7dbf30f
maclandrol <emmanuel.noutahi@hotmail.ca>
2016-02-23 21:58:04 -0500

    Strange behaviour on width attempt fix

--- js/suffixtree.js
+++ js/suffixtree.js
@@ -10,7 +10,7 @@ var treeData = stree.addString('MISSISSIPPI$').convertToJson();
 
 
 // ************** Generate the tree diagram	 *****************
-var margin = {top: 5, right: 25, bottom: 20, left: 120},
+var margin = {top: 5, right: 100, bottom: 20, left: 50},
 	width = 960 - margin.right - margin.left,
 	height = 600 - margin.top - margin.bottom;
 	
